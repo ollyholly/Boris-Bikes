c561c7779e2fabcf6222867aceb181d5cfa5c989
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-17 16:24:42 +0100

    Add solution for steps 1-9

---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+require "bike"
+
+describe Bike do
+  it { respond_to(:working?) }
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,5 @@
+require 'docking_station'
+
+describe DockingStation do
+    it { respond_to(:release_bike) }
+end
