ae73031010f139abe179891e868d39db843d8851
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-17 16:50:45 +0100

    Add work in progress of step 10

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,13 @@ require "bike"
 
 describe Bike do
   it { respond_to(:working?) }
+  # it 'is working?'
+  #   bike = Bike.new
+  #   expect(bike.working?).to eq true
+  # end
+  it 'is working?'
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+  
 end
