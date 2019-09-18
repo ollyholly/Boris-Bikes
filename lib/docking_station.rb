3c9d6e11aecc8f5c0865c54e7f63b57b177347df
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-18 14:37:13 +0100

    Add test for docking and checking the bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'bike'
 class DockingStation
+  attr_accessor :bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def has_bike
+    @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,17 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:has_bike) }
 
   it 'releases a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+  it 'has a bike' do
+    bike = Bike.new
+    station = DockingStation.new
+    station.dock(bike)
+    expect(station.has_bike).to eq(bike)
   end
 end
