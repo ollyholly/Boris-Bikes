eceb5ab889023b197d296575f1646165d39848a3
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-18 15:17:30 +0100

    Add test for limiting capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,19 @@ class DockingStation
   attr_accessor :bike
 
   def release_bike
-    Bike.new
+    if @bike.nil?
+      raise 'there are no bikes'
+    else
+      @bike
+    end
   end
 
   def dock(bike)
-    @bike = bike
+    if @bike
+      raise 'can not dock a bike'
+    else
+      @bike = bike
+    end
   end
 
   def has_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,13 +8,27 @@ describe DockingStation do
   it { is_expected.to respond_to(:has_bike) }
 
   it 'releases a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    docking_station = DockingStation.new
+    docking_station.dock(Bike.new)
+    expect(docking_station.release_bike).to be_working
   end
+
   it 'has a bike' do
     bike = Bike.new
     station = DockingStation.new
     station.dock(bike)
     expect(station.has_bike).to eq(bike)
+  end
+
+  it 'not release bike if not available' do
+    expect { DockingStation.new.release_bike }.to raise_error(RuntimeError, 'there are no bikes')
+  end
+  it 'can not dock bike if there is a bike' do
+    bike1 = Bike.new
+    bike2 = Bike.new
+    station = DockingStation.new
+    station.dock(bike1)
+
+    expect { station.dock(bike2) }.to raise_error(RuntimeError, 'can not dock a bike')
   end
 end
